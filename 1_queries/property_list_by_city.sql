c3ef97479744b6fab25564960cf0344463f802b5
yhupaul <yhupaul@hotmail.com>
2021-08-18 18:33:00 -0700

    create 1_queries

---
+++ 1_queries/property_list_by_city.sql
@@ -0,0 +1,11 @@
+SELECT properties.id,
+  properties.title, 
+  properties.cost_per_night, 
+  avg(property_reviews.rating) as average_rating
+FROM properties
+JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING avg(property_reviews.rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
